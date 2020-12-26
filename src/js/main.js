5ac57357a27876e0c36758d7f522b05eb06e243e
Yazdan Aghaghiri <yazdan.aghaghiri@gmail.com>
2020-12-25 19:22:46 -0800

    Drag circle to destination rect

--- src/js/main.js
+++ src/js/main.js
@@ -13,38 +13,67 @@
   paper.setup('myCanvas');
   var path = new paper.Path();
   var tool = new paper.Tool()
+  const origin =  new paper.Point(100,100)
+  const destination =  new paper.Point(300,100)
 
-		path.strokeColor = 'black';
-		var start = new paper.Point(100, 100);
-		// Move to start and draw a line from there
-		path.moveTo(start);
-		// Note that the plus operator on Point objects does not work
-		// in JavaScript. Instead, we need to call the add() function:
-		path.lineTo(start.add([ 200, -50 ]));
-		// Draw the view now:
-		//view.draw();
-    let circle = new paper.Path.Circle( { 
-        center: new paper.Point(100, 100), 
-        radius: 50,
-        fillColor: 'orange'
-        })
-    circle.strokeColor = 'black';
-		//paper.view.draw();
-   //tool.onMouseDown = (event) => {
-   //  // The mouse was clicked, so let's put a newly created Path into
-   //  // myPath, give it the color black and add the location as the
-   //  // path's first segment.
-   //  myPath = new paper.Path();
-   //  myPath.moveTo(start)
-   //  myPath.strokeColor = 'black';
-   //  myPath.add(event.point);
-   //  paper.view.draw();
-   //  console.log(event)
-   //}
+  let circle = new paper.Path.Circle( { 
+    center: origin, 
+    radius: 50,
+    fillColor: 'orange'
+  })
+  let targRect = new paper.Path.Rectangle({ 
+    center: destination, 
+    size: [100,100]
+  })
+
+  circle.strokeColor = 'black';
+  targRect.strokeColor = 'black';
+  //paper.view.draw();
+  //tool.onMouseDown = (event) => {
+    //  // The mouse was clicked, so let's put a newly created Path into
+    //  // myPath, give it the color black and add the location as the
+    //  // path's first segment.
+    //  myPath = new paper.Path();
+    //  myPath.moveTo(start)
+    //  myPath.strokeColor = 'black';
+    //  myPath.add(event.point);
+    //  paper.view.draw();
+    //  console.log(event)
+    //}
    circle.onMouseDrag = (event) => {
      circle.position = circle.position.add(event.delta)
      //circle.position = circle.position + event.delta
-     console.log(event.delta)
+     //console.log(event.delta)
      //paper.view.draw();
+   }
+   circle.onMouseUp = (event ) => {
+     circle.resolve = true 
+   }
+   paper.view.onFrame = (event) => { 
+     let targ
+     if ( circle.resolve ) {
+     if ( targRect.contains(circle.position)) {
+       targ = destination
+       console.log("YOOHOO")
+     } else {
+       targ = origin
+     }
+       
+     let vector = targ.subtract(circle.position) 
+     let step  
+     console.log('vector length: ' + vector.length )
+     if ( vector.length > 10 ) {
+       step = vector.divide(10)
+       step = step.floor()
+     } else { 
+       step = vector
+     }
+     console.log( 'step is: ' +step)
+     //console.log(circle.position)
+     circle.position = circle.position.add(step)
+     }
+     if ( circle.position.equals(targ) ) {
+       circle.resolve = false
+     }
    }
 
