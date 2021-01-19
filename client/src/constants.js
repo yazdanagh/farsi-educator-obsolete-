9f30e67cea61bf988ec49c3749cdaaff9fc2bc23
Yazdan Aghaghiri <yazdan.aghaghiri@gmail.com>
2021-01-19 08:44:08 -0800

    cleanup constants file

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,38 +2,3 @@
 
 export const canvasWidth = 600;
 export const canvasHeight =300;
-export const alphaGroups = [
-  [ "aa_chasban" ,  "aa_bakola" , "aa_bikola"] ,
-  [ "be_koochik_chap" ,  "be_koochik_dovar",  "be_bozorg_chasban" , "be_bozorg_tanha" ],
-  [ "de_tanha" , "de_chasban" ],
-  [ "me_koochik_chap" , "me_koochik_dovar", "me_bozorg_chasban", "me_bozorg_tanha" ],
-  [ "re_chasban" , "re_tanha" ],
-  [ "ye_koochik_chap", "ye_koochik_dovar" , "ye_bozorg_tanha", "ye_bozorg_chasban" ],
-  [ "ne_koochik_rast", "ne_koochik_dovar" , "ne_bozorg_tanha", "ne_bozorg_chasban" ],
-  [ "sine_chasban_chap", "sine_chasban_dovar" , "sine_bozorg_chasban", "sine_bozorg_tanha" ],
-  //[ "faseleh" ] 
-]
-
-export const darses = [ 
-  [ "be_koochik_chap aa_chasban be_koochik_chap aa_chasban", "baabaa" ], 
-  [ "aa_bakola be_bozorg_tanha", "aab" ],
-  [ "be_koochik_chap aa_chasban de_tanha" , "baad" ],
-  [ "be_koochik_chap aa_chasban me_bozorg_tanha" , "baam" ],
-  [ "de_tanha aa_bikola me_bozorg_tanha" , "daam" ],
-  [ "be_koochik_chap aa_chasban de_tanha aa_bikola me_bozorg_tanha" , "baadaam" ],
-  [ "me_koochik_chap aa_chasban re_tanha" , "maar" ],
-  [ "de_tanha re_tanha" , "dar" ],
-  [ "aa_bakola re_tanha de_tanha" , "aard" ],
-  [ "de_tanha aa_bikola re_tanha aa_bikola" , "daaraa" ],
-  [ "me_koochik_chap aa_chasban de_tanha re_tanha" , "maadar" ], 
-  [ "ye_koochik_chap aa_chasban re_tanha aa_bikola" , "yaara" ], 
-  [ "de_tanha ye_koochik_chap de_chasban"  , "deed" ] , 
-  [ "ne_koochik_rast aa_chasban ne_bozorg_tanha"  , "naan" ] , 
-  [ "aa_bakola be_koochik_chap aa_chasban ne_bozorg_tanha"  , "aabaan" ] , 
-  [ "ne_koochik_rast aa_chasban me_bozorg_tanha"  , "naam" ] , 
-  [ "ne_koochik_rast aa_chasban de_tanha re_tanha"  , "naader" ] , 
-  [ "de_tanha re_tanha sine_bozorg_tanha"  , "dars" ] , 
-  [ "sine_chasban_chap re_chasban de_tanha"  , "sard" ] , 
-  [ "sine_chasban_chap re_chasban me_koochik_chap aa_chasban"  , "sarmaa" ] , 
-  //"be_koochik_chap aa_chasban be_koochik_chap aa_chasban faseleh aa_bakola be_bozorg_tanha faseleh de_tanha aa_bikola de_tanha",
-]
