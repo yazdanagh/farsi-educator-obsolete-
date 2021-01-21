e68be9f64bc2d39875dc59f3586887676f8b639b
Yazdan Aghaghiri <yazdan.aghaghiri@gmail.com>
2021-01-20 20:36:18 -0800

    refactor all server constants in a constants file

---
+++ server/constants.js
@@ -0,0 +1,40 @@
+const darsesInput = [ 
+  [ "be_koochik_chap aa_chasban be_koochik_chap aa_chasban", "baabaa" ],                                         
+  [ "aa_bakola be_bozorg_tanha", "aab" ],
+  [ "be_koochik_chap aa_chasban de_tanha" , "baad" ],
+  [ "be_koochik_chap aa_chasban me_bozorg_tanha" , "baam" ],
+  [ "de_tanha aa_bikola me_bozorg_tanha" , "daam" ],
+  [ "be_koochik_chap aa_chasban de_tanha aa_bikola me_bozorg_tanha" , "baadaam" ],
+  [ "me_koochik_chap aa_chasban re_tanha" , "maar" ],
+  [ "de_tanha re_tanha" , "dar" ],
+  [ "aa_bakola re_tanha de_tanha" , "aard" ],
+  [ "de_tanha aa_bikola re_tanha aa_bikola" , "daaraa" ],
+  [ "me_koochik_chap aa_chasban de_tanha re_tanha" , "maadar" ], 
+  [ "ye_koochik_chap aa_chasban re_tanha aa_bikola" , "yaara" ], 
+  [ "de_tanha ye_koochik_chap de_chasban"  , "deed" ] , 
+  [ "ne_koochik_rast aa_chasban ne_bozorg_tanha"  , "naan" ] , 
+  [ "aa_bakola be_koochik_chap aa_chasban ne_bozorg_tanha"  , "aabaan" ] , 
+  [ "ne_koochik_rast aa_chasban me_bozorg_tanha"  , "naam" ] , 
+  [ "ne_koochik_rast aa_chasban de_tanha re_tanha"  , "naader" ] , 
+  [ "de_tanha re_tanha sine_bozorg_tanha"  , "dars" ] , 
+  [ "sine_chasban_chap re_chasban de_tanha"  , "sard" ] , 
+  [ "sine_chasban_chap re_chasban me_koochik_chap aa_chasban"  , "sarmaa" ] 
+]
+
+
+const harfInput = [
+  "aa aa_bakola aa_chasban aa_bikola",
+  "be be_bozorg_tanha be_koochik_chap be_koochik_dovar be_bozorg_chasban" ,
+  "de de_tanha de_chasban" ,
+  "me me_bozorg_tanha me_koochik_chap me_koochik_dovar me_bozorg_chasban" ,
+  "re re_tanha re_chasban" ,
+  "ye ye_bozorg_tanha ye_koochik_chap ye_koochik_dovar ye_bozorg_chasban" ,
+  "ne ne_bozorg_tanha ne_koochik_rast ne_koochik_dovar ne_bozorg_chasban" ,
+  "sin sine_chasban_chap sine_chasban_dovar sine_bozorg_chasban sine_bozorg_tanha" ,
+  //[ faseleh ] 
+]
+
+module.exports = {
+  harfInput,
+  darsesInput
+}
--- server/process_darses.js
+++ server/process_darses.js
@@ -6,18 +6,8 @@ const exec = util.promisify(require('child_process').exec);
 const fs = bluebird.promisifyAll(require("fs-extra"))
 const _= require("lodash")
 const db = require("./mongo.js")
-
-  const harfInput = [
-   "alef aa_bakola aa_chasban aa_bakola aa_bikola"  ,
-   "be be_bozorg_tanha be_koochik_chap  be_koochik_dovar be_bozorg_chasban be_bozorg_tanha" ,
-   "de de_tanha de_chasban" ,
-   "me me_bozorg_tanha me_koochik_chap me_koochik_dovar  me_bozorg_chasban" ,
-   "re re_tanha re_chasban" ,
-   "ye ye_bozorg_tanha ye_koochik_chap ye_koochik_dovar ye_bozorg_chasban" ,
-   "ne ne_bozorg_tanha ne_koochik_rast ne_koochik_dovar ne_bozorg_chasban" ,
-   "sin sine_chasban_chap sine_chasban_dovar sine_bozorg_chasban sine_bozorg_tanha" ,
-  //[ faseleh ] 
-]
+const cons = require('./constants');
+  
 
 
   const main = async () => {
@@ -37,7 +27,7 @@ const db = require("./mongo.js")
     console.log(darsesDB)
 
     
-    const harves = harfInput.map( a => { 
+    const harves = cons.harfInput.map( a => { 
       const elems = a.split(' ')
       return { 
         harfName: elems[0],
