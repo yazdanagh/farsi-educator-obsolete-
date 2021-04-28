af40f7eb1ef4990305094b6334fce8ea2f99f8e5
Yazdan Aghaghiri <yazdan.aghaghiri@gmail.com>
2021-04-27 22:22:29 -0700

    Show 5 dars per page in all darses

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -225,7 +225,7 @@ module.exports = (app) => {
       const numDarses = student.name === 'test' ? totDarses + 1 : student.darsId + 1   
       console.log('+++++++++++' + totDarses + student + studentId )
       //const dars = await db.dars.findOne({darsId: student.darsId})   
-      const darses = await db.dars.find({darsId: { $in:  Array.from(Array(numDarses).keys())  }},{},{ sort: {darsId: 1}, skip: (page-1)*10, limit: 10})   
+      const darses = await db.dars.find({darsId: { $in:  Array.from(Array(numDarses).keys())  }},{},{ sort: {darsId: 1}, skip: (page-1)*5, limit: 5})   
       if ( darses ) {
         res.json(darses)
       } else {
