b34d65860dc745981f9e519451df137c4404adb6
Yazdan Aghaghiri <yazdan.aghaghiri@gmail.com>
2021-09-06 23:10:47 -0700

    modify constants file and github token

--- readme.txt
+++ readme.txt
@@ -1,5 +1,37 @@
+
+
+1. Running the app
+
+building client
+( cd ../client; npm run build )
+
+running server
 ~/.nvm/versions/node/v15.5.1/bin/node index.js >& server.log &
 
+loading db:
+node load_darses.js
+
+2. forward port 80 traffic to port 8000
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
 
+3. Running mongo db
+
+mongodb user creation
+running: brew services start mongodb-community@4.4
+use shekkar
+db.createUser({user: "yazdan", pwd: "my_pwd", roles: ["dbOwner"] })
+
+4. admin account 
+
+An admin account is needed with code 1111 and studentId : 1000 and name: admin
+
+
+
+inside mongo shell:
+use shekkar ||||| db.students.insert([ { name: 'Yara Aghaghiri',        naam: 'ﯼﺍﺭﺍ ﻊﻗﺎﻘﯾﺮﯾ',        darsId: 16,        email: 'yazdan.aghaghiri@gmail.com',        code: 8318 },      { name: 'Delsa Aghaghiri',        naam: 'ﺪﻠﺳﺍ ﻊﻗﺎﻘﯾﺮﯾ',        darsId: 11,        email: 'yazdan.aghaghiri@gmail.com',        code: 8173 }])
+
+
+
+5. git hub
+access token: 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
--- server/constants.js
+++ server/constants.js
@@ -1,304 +1,310 @@
 const darsesInput = [
 
   // alef & be 
-  [ "aa_bakola be_bozorg_tanha", "aab" ],
-  [ "be_koochik_chap aa_chasban be_koochik_chap aa_chasban", "baabaa" ],                                         
+   [ "aabia bbt", "aab" ],
+ //[ "aabia be_bozorg_tanha", "aab" ],
+ //[ "be_koochik_chap aa_chasban be_koochik_chap aa_chasban", "baabaa" ],                                         
+ //[ "be_koochik_chap aa_chasban be_koochik_chap aa_chasban", "baabaa" ],                                         
 
   // de
-  [ "be_koochik_chap aa_chasban de_tanha" , "baad" ],
-  [ "de_tanha aa_bikola de_tanha" , "daad" ],
-  [ "aaba bkc aac dt", "aabaad" ],
-  [ "aaba dt aabi bbt", "aadaab" ],
-
-
-  // meem 
-  [ "be_koochik_chap aa_chasban me_bozorg_tanha" , "baam" ],
-  [ "de_tanha aa_bikola me_bozorg_tanha" , "daam" ],
-  [ "be_koochik_chap aa_chasban de_tanha aa_bikola me_bozorg_tanha" , "baadaam" ],
-  [ "aaba mkc dc", "aamad" ],
-  [ "aaba dt mbt", "aadam" ],
-
-  // re 
-  [ "me_koochik_chap aa_chasban de_tanha re_tanha" , "maadar" ], 
-  [ "aa_bakola re_tanha de_tanha" , "aard" ],
-  [ "me_koochik_chap aa_chasban re_tanha" , "maar" ],
-  [ "de_tanha aa_bikola re_tanha aa_bikola" , "daaraa" ],
-  [ "bkc rc aabi dt rt" , "baraadar" ],
-  [ "de_tanha re_tanha" , "dar" ],
-  [ "de_tanha aa_bikola re_tanha me_bozorg_tanha" , "daaram" ], 
-
-  // noon 
-  [ "ne_koochik_chap aa_chasban ne_bozorg_tanha"  , "naan" ] , 
-  [ "aa_bakola be_koochik_chap aa_chasban ne_bozorg_tanha"  , "aabaan" ] , 
-  [ "bkc aac rt aabi nbt "  , "baaraan" ] , 
-  [ "ne_koochik_chap aa_chasban me_bozorg_tanha"  , "naam" ] , 
-  [ "ne_koochik_chap aa_chasban de_tanha re_tanha"  , "naader" ] , 
-  [ "aabi nkc aac rt"  , "anaar" ] , 
-  [ "mkc nbc"  , "man" ] , 
-
-
-  // ye
-  [ "ye_koochik_chap aa_chasban de_tanha" , "yaad" ], 
-  [ "ye_koochik_chap aa_chasban re_tanha aa_bikola" , "yaara" ], 
-  [ "dt rt ykc aac" , "daryaa" ], 
-  [ "aaba bkc ybc"  , "aabee" ] , 
-  [ "be_koochik_chap ye_koochik_dovar ne_koochik_dovar ye_bozorg_chasban"  , "beenee" ] ,
-  [ "aabi ykc rc aabi nbt "  , "eeraan" ] , 
-  [ "de_tanha ye_koochik_chap de_chasban"  , "deed" ] , 
-  
-  
-  // sin
-  [ "de_tanha re_tanha sine_bozorg_tanha"  , "dars" ] , 
-  [ "sine_koochik_chap re_chasban de_tanha"  , "sard" ] , 
-  [ "sine_koochik_chap re_chasban me_koochik_chap aa_chasban"  , "sarmaa" ] ,
-  [ "sikc ykd nkd ybc" , "seenee" ] ,
-  //[ "dt aabi rt sikc ybc"  , "daarsee" ] ,
-  [ "aabi sikc bbc"  , "asb" ] ,
-  [ "sikc ykd bbc"  , "seeb" ] ,
-
-  // pe
-  [ "pkc aac" , "paa" ] ,
-  [ "pkc dc rt" , "pedar" ] ,
-  [ "pkc nkd ykd rc" , "paneer" ] ,
-  [ "pkc aac rt sikc ybc" , "paarsee" ] ,
-  [ "pkc aac rt sikc aac" , "paarsaa" ] ,
-  [ "pkc rc ykc dc nbt" , "pareedan" ] ,
-
-  // te
-  [ "tkc aac rt aabi" , "taaraa" ] ,
-  [ "bkc ykd sikd tbc" , "beest" ] ,
-  [ "dt sikc tbc" , "dast" ] ,
-  [ "mkc aac sikc tbc" , "maast" ] ,
-  [ "rt aabi sikc tbc" , "raast" ] ,
-  [ "dt aabi sikc tkd aac nbt" , "daastaan" ] ,
-  [ "bkc sikd tkd nkd ybc"  , "bastanee" ] ,
-
-  // shin
-  [ "shikc aac dt ybt", "shaadee" ],
-  [ "shikc aac mbt", "shaam" ],
-  [ "aaba tkc shibc", "aatash" ],
-  [ "aaba rt shibt", "aarash" ],
-  [ "rt ykc shibc", "reesh" ],
-  [ "shikc ykd rc", "sheer" ],
-  [ "shikc mkd aac", "shoma" ],
-
-  // ze
-  [ "bkc aac zt ybt", "baazee" ], 
-  [ "zt rt dt", "zard" ], 
-  [ "zt nbt", "zan" ], 
-  [ "sikc aac zt", "saaz" ], 
-  [ "bkc zc", "boz" ], 
-  [ "nkc aac zt nkc ykd nbc", "nazaneen" ], 
-  [ "sikc bkd zc", "sabz" ], 
-  [ "aaba zt aabi dt ybt", "aazaadee" ], 
-
-
-  // khe
-  [ "dt rt khkc tbc", "derakht" ],
-  [ "sikc khkd tbc", "sakht" ],
-  [ "nkc khbc", "nakh" ],
-  [ "tkc khkd tbc", "takht" ],
-  [ "sikc aac khkc tbc", "saakht" ],
-
-
-  // che
-  [ "chkc nkd aac rt", "chenaar" ],
-  [ "mkc chbc", "moch" ],
-  [ "chkc rc bbt", "charb" ],
-  [ "pkc rc chkc mbc", "parcham" ],
-  [ "chkc shikd mbc", "cheshm" ],
-
-
-  // kaf
-  [ "kkc aac rt", "kaar" ],
-  [ "bkc aac dt kkc nkd kbc", "baadkonak" ],
-  [ "kkc nkd dc", "kand" ],
-  [ "kkc ykd kbc", "keik" ],
-  [ "shikc aac pkc rc kbt", "shaaparak" ],
-  [ "kkc rc mbt", "kerm" ],
-  [ "kkc mkd kbc", "komak" ],
-  [ "khkc aac kbt", "khaak" ],
-
-  // gaf
-  [ "rt nkc gbc", "rang" ],
-  [ "bkc rc gbt", "barg" ],
-  [ "gkc rc gbt", "gorg" ],
-  [ "sikc gbc", "sag" ],
-  [ "gkc rc mbt", "garm" ],
-  [ "sikc nkd gbc", "sang" ],
-  [ "rt nkc gbc", "rang" ],
-
-  
-  // le
-  [ "dt lkc sikd aac", "delsaa" ], 
-  [ "bkc aac lbt", "baal" ], 
-  [ "bkc aac lkc shibc", "baalesh" ], 
-  [ "lkc aac zt mbt", "laazem" ], 
-  [ "bkc lkd aac lbt", "balaal" ], 
-  ["bkc lkd nkd dc", "boland"],
-  ["lkc bbc", "lab"],
-  ["khkc aac lkc ybc", "khalee"],
-  [ "chkc nkd gkd aac lbt", "changaal" ],
-  [ "pkc lkd nkd gbc", "palang" ],
-  
-  // fe
-  ["bkc rc fbt", "barf"],
-  ["fkc ykd lbc", "feel"],
-  ["fkc rc dt aabi", "fardaa"],
-  ["fkc rc ykc aac dt", "faryaad"],
-  
-  
-  // ghaf
-  ["ghkc nkd dc", "ghand"],
-  ["ghkc aac shikc ghbc", "ghaashogh"],
-  //["chkc aac ghbt", "chaagh"],
-  ["bkc shikd ghkd aac bbt", "boshghaab"],
-  ["ghkc rc mkc zc", "ghermez"],
-  ["ghkc bkd lbc", "ghabl"],
-
-  // vav
-  ["sikc vc zt nbt", "soozan"],
-  ["zt vt rt", "zoor"],
-  ["dt vt rt", "door"],
-  ["mkc vc shibt", "moosh"],
-  ["aaba vt aabi zt", "aavaaz"],
-  ["nkc vc aabi", "navaa"],
-  ["mkc vc zc", "moz"],
-  [ "pkc lkd vc", "polo" ], 
-  [ "aaba lkc vc", "aaloo" ], 
-  [ "vt aabi nkc ykd lbc", "vaaneel" ], 
-
-  // jim
-  [ "aabi ykc nkd jkd aac", "eenja" ],
-  [ "gkc nkd jbc", "ganj" ],
-  [ "jkc aac nbt", "jaan" ],
-  [ "jkc ykd kbc jkc ykd kbc", "jeek jeek" ],
-  [ "jkc aac dt vt", "jaadoo" ],
-
-  // he
-  [ "hkc mkd aac mbt", "hamaam" ],
-  [ "hkc rc kkc tbc", "harekat" ],
-  [ "hkc mkd ykd dc", "hameed" ],
-  [ "hkc aac lbt", "haal" ],
-  [ "mkc hkd lbc", "mahal" ],
-
-  // hhe
-  ["mkc aac hhat" , "maah" ],
-  ["rt aabi hhat" , "raah" ],
-  ["khkc nkd dc hhat" , "khandeh" ],
-  ["khkc aac lkc hhac" , "khaleh" ],
-  ["khkc aac nkc hhac" , "khaneh" ],
-  ["zt rt aabi fkc hhac" , "zarafeh" ],
-  ["mkc hhv mbc" , "mohem" ],
-  ["hha mkd ykd shikd hhac" , "hamisheh" ],
-
-  // zhe
-  ["zht aabi kkc tbc" , "zhaakat" ],
-  ["mkc aac zht ykc kbc" , "mazheek" ],
-  ["bkc ykd zhc nbt" , "beezhan" ],
-
-  // sad
-  [ "sadekc dc aabi", "sedaa" ],
-  [ "sadekc dc fbt", "sadaf" ],
-  [ "sadekc aac fbt", "saaf" ],
-  [ "sadekc nkd dc lkc ybc", "sandalee" ],
-
-  // zze
-  [ "aaba zzt rt", "aazar" ],
-  [ "lkc zzc tbt", "lezat" ],
-  [ "gkc zzc shikc tbc", "gozaasht" ],
-  [ "zzt rt tbt", "zorat" ],
-
-  // ein
-  [ "einekc lkd ybc", "ali" ],
-  [ "einekc mkd hhac", "ameh" ],
-  [ "einekc mkd vc", "amoo" ],
-  [ "einekc zc ykc zc", "azeez" ],
-  [ "shikc mkd einebc", "shamea" ],
-  [ "einekc ykd dc ybt", "eidee"],
-  [ "shikc rc vt einebt", "shoroo"],
-
-  // sse
-  [ "kkc sskd ykd fbc", "kaseef"],
-  [ "lkc sskd hhac", "laseh"],
-  [ "sskc aac nkc ykd hhac", "sanieh"],
-
-  // zad
-  [ "mkc rc ykc zadebc", "mareez"],
-  [ "rt zadekc aac", "reza"],
-  [ "einekc vc zadebt", "avaz"],
-
-  // ghein
-  [ "gheinekc mbc", "gham"],
-  [ "kkc lkd aac gheinebt", "khalaagh"],
-  [ "bkc aac gheinebt", "baagh"],
-  [ "mkc rc gheinebt", "morgh"],
-  [ "jkc gheinekd dc", "joghd"],
-  [ "gheinekc aac zt", "ghaaz"],
-  [ "gheinekc rc vt bbt", "ghoroob"],
-
-  // ta
-  [ "ghkc vc tac ybc", "ghootee"],
-  [ "tac aac lkc bkd ybc", "taalebee" ],
-  [ "tac vc tac ybc", "tootee" ],
-  [ "vt sikc tat", "vasat" ],
-  [ "vt tac nbc", "vatan" ],
-  [ "hkc ykd aac tat", "hayaat" ],
-  
-  // za
-  [ "zac rc fbt", "zarf" ],
-  [ "zac hhv rc", "zohr" ],
-  [ "nkc zad mbc", "nazm" ],
-  [ "hkc aac fkc zar", "haafez" ],
-  [ "lkc hkd zad hhac", "lahzeh" ],
-  [ "khkc dc aabi hkc aac fkc zar", "khodahafez" ],
-  [ "mkc vc aabi zac bbc", "movazeb" ],
+// [ "be_koochik_chap aa_chasban de_tanha" , "baad" ],
+// [ "de_tanha aa_bikola de_tanha" , "daad" ],
+// [ "aaba bkc aac dt", "aabaad" ],
+// [ "aaba dt aabi bbt", "aadaab" ],
+//
+//
+// // meem 
+// [ "be_koochik_chap aa_chasban me_bozorg_tanha" , "baam" ],
+// [ "de_tanha aa_bikola me_bozorg_tanha" , "daam" ],
+// [ "be_koochik_chap aa_chasban de_tanha aa_bikola me_bozorg_tanha" , "baadaam" ],
+// [ "aaba mkc dc", "aamad" ],
+// [ "aaba dt mbt", "aadam" ],
+//
+// // re 
+// [ "me_koochik_chap aa_chasban de_tanha re_tanha" , "maadar" ], 
+// [ "aa_bakola re_tanha de_tanha" , "aard" ],
+// [ "me_koochik_chap aa_chasban re_tanha" , "maar" ],
+// [ "de_tanha aa_bikola re_tanha aa_bikola" , "daaraa" ],
+// [ "bkc rc aabi dt rt" , "baraadar" ],
+// [ "de_tanha re_tanha" , "dar" ],
+// [ "de_tanha aa_bikola re_tanha me_bozorg_tanha" , "daaram" ], 
+//
+// // noon 
+// [ "ne_koochik_chap aa_chasban ne_bozorg_tanha"  , "naan" ] , 
+// [ "aa_bakola be_koochik_chap aa_chasban ne_bozorg_tanha"  , "aabaan" ] , 
+// [ "bkc aac rt aabi nbt "  , "baaraan" ] , 
+// [ "ne_koochik_chap aa_chasban me_bozorg_tanha"  , "naam" ] , 
+// [ "ne_koochik_chap aa_chasban de_tanha re_tanha"  , "naader" ] , 
+// [ "aabi nkc aac rt"  , "anaar" ] , 
+// [ "mkc nbc"  , "man" ] , 
+//
+//
+// // ye
+// [ "ye_koochik_chap aa_chasban de_tanha" , "yaad" ], 
+// [ "ye_koochik_chap aa_chasban re_tanha aa_bikola" , "yaara" ], 
+// [ "dt rt ykc aac" , "daryaa" ], 
+// [ "aaba bkc ybc"  , "aabee" ] , 
+// [ "be_koochik_chap ye_koochik_dovar ne_koochik_dovar ye_bozorg_chasban"  , "beenee" ] ,
+// [ "aabi ykc rc aabi nbt "  , "eeraan" ] , 
+// [ "de_tanha ye_koochik_chap de_chasban"  , "deed" ] , 
+// 
+// 
+// // sin
+// [ "de_tanha re_tanha sine_bozorg_tanha"  , "dars" ] , 
+// [ "sine_koochik_chap re_chasban de_tanha"  , "sard" ] , 
+// [ "sine_koochik_chap re_chasban me_koochik_chap aa_chasban"  , "sarmaa" ] ,
+// [ "sikc ykd nkd ybc" , "seenee" ] ,
+// //[ "dt aabi rt sikc ybc"  , "daarsee" ] ,
+// [ "aabi sikc bbc"  , "asb" ] ,
+// [ "sikc ykd bbc"  , "seeb" ] ,
+//
+// // pe
+// [ "pkc aac" , "paa" ] ,
+// [ "pkc dc rt" , "pedar" ] ,
+// [ "pkc nkd ykd rc" , "paneer" ] ,
+// [ "pkc aac rt sikc ybc" , "paarsee" ] ,
+// [ "pkc aac rt sikc aac" , "paarsaa" ] ,
+// [ "pkc rc ykc dc nbt" , "pareedan" ] ,
+//
+// // te
+// [ "tkc aac rt aabi" , "taaraa" ] ,
+// [ "bkc ykd sikd tbc" , "beest" ] ,
+// [ "dt sikc tbc" , "dast" ] ,
+// [ "mkc aac sikc tbc" , "maast" ] ,
+// [ "rt aabi sikc tbc" , "raast" ] ,
+// [ "dt aabi sikc tkd aac nbt" , "daastaan" ] ,
+// [ "bkc sikd tkd nkd ybc"  , "bastanee" ] ,
+//
+// // shin
+// [ "shikc aac dt ybt", "shaadee" ],
+// [ "shikc aac mbt", "shaam" ],
+// [ "aaba tkc shibc", "aatash" ],
+// [ "aaba rt shibt", "aarash" ],
+// [ "rt ykc shibc", "reesh" ],
+// [ "shikc ykd rc", "sheer" ],
+// [ "shikc mkd aac", "shoma" ],
+//
+// // ze
+// [ "bkc aac zt ybt", "baazee" ], 
+// [ "zt rt dt", "zard" ], 
+// [ "zt nbt", "zan" ], 
+// [ "sikc aac zt", "saaz" ], 
+// [ "bkc zc", "boz" ], 
+// [ "nkc aac zt nkc ykd nbc", "nazaneen" ], 
+// [ "sikc bkd zc", "sabz" ], 
+// [ "aaba zt aabi dt ybt", "aazaadee" ], 
+//
+//
+// // khe
+// [ "dt rt khkc tbc", "derakht" ],
+// [ "sikc khkd tbc", "sakht" ],
+// [ "nkc khbc", "nakh" ],
+// [ "tkc khkd tbc", "takht" ],
+// [ "sikc aac khkc tbc", "saakht" ],
+//
+//
+// // che
+// [ "chkc nkd aac rt", "chenaar" ],
+// [ "mkc chbc", "moch" ],
+// [ "chkc rc bbt", "charb" ],
+// [ "pkc rc chkc mbc", "parcham" ],
+// [ "chkc shikd mbc", "cheshm" ],
+//
+//
+// // kaf
+// [ "kkc aac rt", "kaar" ],
+// [ "bkc aac dt kkc nkd kbc", "baadkonak" ],
+// [ "kkc nkd dc", "kand" ],
+// [ "kkc ykd kbc", "keik" ],
+// [ "shikc aac pkc rc kbt", "shaaparak" ],
+// [ "kkc rc mbt", "kerm" ],
+// [ "kkc mkd kbc", "komak" ],
+// [ "khkc aac kbt", "khaak" ],
+//
+// // gaf
+// [ "rt nkc gbc", "rang" ],
+// [ "bkc rc gbt", "barg" ],
+// [ "gkc rc gbt", "gorg" ],
+// [ "sikc gbc", "sag" ],
+// [ "gkc rc mbt", "garm" ],
+// [ "sikc nkd gbc", "sang" ],
+// [ "rt nkc gbc", "rang" ],
+//
+// 
+// // le
+// [ "dt lkc sikd aac", "delsaa" ], 
+// [ "bkc aac lbt", "baal" ], 
+// [ "bkc aac lkc shibc", "baalesh" ], 
+// [ "lkc aac zt mbt", "laazem" ], 
+// [ "bkc lkd aac lbt", "balaal" ], 
+// ["bkc lkd nkd dc", "boland"],
+// ["lkc bbc", "lab"],
+// ["khkc aac lkc ybc", "khalee"],
+// [ "chkc nkd gkd aac lbt", "changaal" ],
+// [ "pkc lkd nkd gbc", "palang" ],
+// 
+// // fe
+// ["bkc rc fbt", "barf"],
+// ["fkc ykd lbc", "feel"],
+// ["fkc rc dt aabi", "fardaa"],
+// ["fkc rc ykc aac dt", "faryaad"],
+// 
+// 
+// // ghaf
+// ["ghkc nkd dc", "ghand"],
+// ["ghkc aac shikc ghbc", "ghaashogh"],
+// //["chkc aac ghbt", "chaagh"],
+// ["bkc shikd ghkd aac bbt", "boshghaab"],
+// ["ghkc rc mkc zc", "ghermez"],
+// ["ghkc bkd lbc", "ghabl"],
+//
+// // vav
+// ["sikc vc zt nbt", "soozan"],
+// ["zt vt rt", "zoor"],
+// ["dt vt rt", "door"],
+// ["mkc vc shibt", "moosh"],
+// ["aaba vt aabi zt", "aavaaz"],
+// ["nkc vc aabi", "navaa"],
+// ["mkc vc zc", "moz"],
+// [ "pkc lkd vc", "polo" ], 
+// [ "aaba lkc vc", "aaloo" ], 
+// [ "vt aabi nkc ykd lbc", "vaaneel" ], 
+//
+// // jim
+// [ "aabi ykc nkd jkd aac", "eenja" ],
+// [ "gkc nkd jbc", "ganj" ],
+// [ "jkc aac nbt", "jaan" ],
+// [ "jkc ykd kbc jkc ykd kbc", "jeek jeek" ],
+// [ "jkc aac dt vt", "jaadoo" ],
+//
+// // he
+// [ "hkc mkd aac mbt", "hamaam" ],
+// [ "hkc rc kkc tbc", "harekat" ],
+// [ "hkc mkd ykd dc", "hameed" ],
+// [ "hkc aac lbt", "haal" ],
+// [ "mkc hkd lbc", "mahal" ],
+//
+// // hhe
+// ["mkc aac hhat" , "maah" ],
+// ["rt aabi hhat" , "raah" ],
+// ["khkc nkd dc hhat" , "khandeh" ],
+// ["khkc aac lkc hhac" , "khaleh" ],
+// ["khkc aac nkc hhac" , "khaneh" ],
+// ["zt rt aabi fkc hhac" , "zarafeh" ],
+// ["mkc hhv mbc" , "mohem" ],
+// ["hha mkd ykd shikd hhac" , "hamisheh" ],
+//
+// // zhe
+// ["zht aabi kkc tbc" , "zhaakat" ],
+// ["mkc aac zht ykc kbc" , "mazheek" ],
+// ["bkc ykd zhc nbt" , "beezhan" ],
+//
+// // sad
+// [ "sadekc dc aabi", "sedaa" ],
+// [ "sadekc dc fbt", "sadaf" ],
+// [ "sadekc aac fbt", "saaf" ],
+// [ "sadekc nkd dc lkc ybc", "sandalee" ],
+//
+// // zze
+// [ "aaba zzt rt", "aazar" ],
+// [ "lkc zzc tbt", "lezat" ],
+// [ "gkc zzc shikc tbc", "gozaasht" ],
+// [ "zzt rt tbt", "zorat" ],
+//
+// // ein
+// [ "einekc lkd ybc", "ali" ],
+// [ "einekc mkd hhac", "ameh" ],
+// [ "einekc mkd vc", "amoo" ],
+// [ "einekc zc ykc zc", "azeez" ],
+// [ "shikc mkd einebc", "shamea" ],
+// [ "einekc ykd dc ybt", "eidee"],
+// [ "shikc rc vt einebt", "shoroo"],
+//
+// // sse
+// [ "kkc sskd ykd fbc", "kaseef"],
+// [ "lkc sskd hhac", "laseh"],
+// [ "sskc aac nkc ykd hhac", "sanieh"],
+//
+// // zad
+// [ "mkc rc ykc zadebc", "mareez"],
+// [ "rt zadekc aac", "reza"],
+// [ "einekc vc zadebt", "avaz"],
+//
+// // ghein
+// [ "gheinekc mbc", "gham"],
+// [ "kkc lkd aac gheinebt", "khalaagh"],
+// [ "bkc aac gheinebt", "baagh"],
+// [ "mkc rc gheinebt", "morgh"],
+// [ "jkc gheinekd dc", "joghd"],
+// [ "gheinekc aac zt", "ghaaz"],
+// [ "gheinekc rc vt bbt", "ghoroob"],
+//
+// // ta
+// [ "ghkc vc tac ybc", "ghootee"],
+// [ "tac aac lkc bkd ybc", "taalebee" ],
+// [ "tac vc tac ybc", "tootee" ],
+// [ "vt sikc tat", "vasat" ],
+// [ "vt tac nbc", "vatan" ],
+// [ "hkc ykd aac tat", "hayaat" ],
+// 
+// // za
+// [ "zac rc fbt", "zarf" ],
+// [ "zac hhv rc", "zohr" ],
+// [ "nkc zad mbc", "nazm" ],
+// [ "hkc aac fkc zar", "haafez" ],
+// [ "lkc hkd zad hhac", "lahzeh" ],
+// [ "khkc dc aabi hkc aac fkc zar", "khodahafez" ],
+// [ "mkc vc aabi zac bbc", "movazeb" ],
 
 ]
 
 
 // harfName,
-//       harfSound key harFrom(harfLead), 
-//       harfSound key harfForm  
+//       harfSound HarfKey harFrom(harfLead) primaryKey
+//       harfSound HarfKey harfForm primaryKey  
+//       if primaryKey is self
+//           harfKey renders as itself in alphaTile and resolve as itself in placeHolderInst 
+//       else 
+//           harfKey renders as primaryKey in alphaTile and resolve ast itself in placeHolderInst
 const harfInput = [
   `alef 
-       aa aaba aa_bakola 
-       aa aac aa_chasban 
-       aa aabi aa_bikola 
-       aa_a aabia aa_bikola_a 
-       aa_e aabio aa_bikola_e 
-       aa_o aabio aa_bikola_o`,
+       aa aaba aa_bakola self
+       aa aac aa_chasban self
+       aa aabi aa_bikola self
+       aa_a aabia aa_bikola_a aabi
+       aa_e aabie aa_bikola_e aabi
+       aa_o aabio aa_bikola_o aabi`,
   `be 
-       be bbt be_bozorg_tanha 
-       be bkc be_koochik_chap 
-       be_a bkca _ 
-       be_e bkce _ 
-       be_o bkco _ 
-       be bkd be_koochik_dovar 
-       be_a bkda _ 
-       be_e bkde _ 
-       be_o bkdo _ 
-       be bbc be_bozorg_chasban`,
-  `de 
-       de dt de_tanha 
-       de_a dta _ 
-       de_e dte _ 
-       de_o dto _ 
-       de dc de_chasban 
-       de_a dca _ 
-       de_e dce _ 
-       de_o dco _` ,
-  `meem 
-       me mbt me_bozorg_tanha 
-       me mkc me_koochik_chap 
-       me_a mkca _ 
-       me_e mkce _ 
-       me_o mkco _ 
-       mkd me_koochik_dovar 
-       me_a mkda _ 
-       me_e mkde _ 
-       me_o mkdo _ 
-       me mbc me_bozorg_chasban`,
+       be bbt be_bozorg_tanha self 
+       be bkc be_koochik_chap self 
+       be_a bkca _ bkc 
+       be_e bkce _ bkc 
+       be_o bkco _ bkc 
+       be bkd be_koochik_dovar self 
+       be_a bkda _ bkd 
+       be_e bkde _ bkd 
+       be_o bkdo _ bkd 
+       be bbc be_bozorg_chasban self`,
+// `de 
+//      de dt de_tanha 
+//      de_a dta _ 
+//      de_e dte _ 
+//      de_o dto _ 
+//      de dc de_chasban 
+//      de_a dca _ 
+//      de_e dce _ 
+//      de_o dco _` ,
+// `meem 
+//      me mbt me_bozorg_tanha 
+//      me mkc me_koochik_chap 
+//      me_a mkca _ 
+//      me_e mkce _ 
+//      me_o mkco _ 
+//      mkd me_koochik_dovar 
+//      me_a mkda _ 
+//      me_e mkde _ 
+//      me_o mkdo _ 
+//      me mbc me_bozorg_chasban`,
   //"re re rt re_tanha rc re_chasban" ,
   //"noon ne nbt  ne_bozorg_tanha nkc  ne_koochik_chap nkd ne_koochik_dovar nbc ne_bozorg_chasban" ,
   //"ye ye ybt  ye_bozorg_tanha ykc ye_koochik_chap ykd ye_koochik_dovar  ybc ye_bozorg_chasban" ,
